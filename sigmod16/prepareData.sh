a09561b8ea827bcbc341a659d157da6a7e3d4c76
Anique <aniquetahir@gmail.com>
2017-09-05 04:05:14 +0000

    yellow cab data specific changes

--- sigmod16/prepareData.sh
+++ sigmod16/prepareData.sh
@@ -1,11 +1,13 @@
 ## Merge tree index performance, correctness and robustness
-./download-time-series
+#./download-time-series
 ## Others
 cd setup/
 ./hdfs_dir
-./load_nyc_urban
-./load_nyc_open
+./load_yellowdata
+#./load_nyc_urban
+#./load_nyc_open
 ## Pre-processing
 cd ../pre-processing/
-./pre-processing-nyc-urban
-./pre-processing-nyc-open
+./pre-processing-yellowdata
+#./pre-processing-nyc-urban
+#./pre-processing-nyc-open
